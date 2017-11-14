fa51f6a7de27ded4c1ddf60b96ffe86a5993d9a7
Dmitry Peshkin <dmitrey04@mail.ru>
2017-11-14 22:18:23 +0500

    app pass tests

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import PersonalForm from "./PersonalForm";
 import CardForm from "./CardForm";
 
 const stepTitles = ["Personal information", "Card information", "Finish"];
-let id = 0;
-function getNewId() {
-  id += 1;
-  return id;
-}
+
 class App extends Component {
   state = {
     step: 1,
@@ -20,41 +16,97 @@ class App extends Component {
     isTimeOver: false
   };
 
-  handleTabClick = () => {};
+  handleTabClick = step => {
+    this.setState({ step });
+  };
 
-  handleChangeForm = () => {};
+  handleChangeForm = (field, value) => {
+    this.setState({ [field]: value });
+  };
 
-  handleClickNextForm = () => {};
+  handleClickNextForm = () => {
+    const step = this.state.step;
+    this.setState({ step: step + 1 });
+  };
 
-  handleChangeTimeOver = () => {};
+  handleChangeTimeOver = value => {
+    this.setState({ isTimeOver: value });
+  };
 
-  isFormCommitable = () => {};
+  isFormCommitable = () => {
+    const { step, firstName, lastName, email, cardNumber } = this.state;
+    switch (step) {
+      case 1:
+        if (
+          firstName !== "" &&
+          lastName !== "" &&
+          email !== "" &&
+          email.includes("@")
+        )
+          return true;
+        break;
+      case 2:
+        if (cardNumber.length === 16) return true;
+        break;
+      default:
+        return false;
+    }
+  };
 
-  renderForm = () => {};
-
-  checkStep = () => {};
+  renderForm = () => {
+    const { step, firstName, lastName, email, cardNumber } = this.state;
+    switch (step) {
+      case 1:
+        return (
+          <PersonalForm
+            firstName={firstName}
+            lastName={lastName}
+            email={email}
+            onChangeForm={this.handleChangeForm}
+          />
+        );
+      case 2:
+        return (
+          <CardForm
+            cardNumber={cardNumber}
+            onChangeForm={this.handleChangeForm}
+            onChangeTimeOver={this.handleChangeTimeOver}
+          />
+        );
+      case 3:
+        return "Поздравляем!";
+      default:
+        return alert("Здесь должна быть форма");
+    }
+  };
 
   render() {
+    const { step, isTimeOver } = this.state;
+
     return (
       <div className="container">
-        <ul className="tab-panel">
-          {stepTitles.map((step, index) => (
-            <li key={getNewId()}>
-              <Step
-                title={step}
-                onClick={this.handleTabClick}
-                isSelected={true}
-                number={index + 1}
-                isClickable={false}
-              />
-            </li>
+        <div className="tab-panel">
+          {stepTitles.map((title, index) => (
+            <Step
+              key={title}
+              onClick={this.handleTabClick}
+              isSelected={index + 1 === step}
+              number={index + 1}
+              isClickable={index + 1 < step}
+            >
+              {title}
+            </Step>
           ))}
-        </ul>
-        <div className="form-content">
-          <PersonalForm />
         </div>
+        <div className="form-content">{this.renderForm()}</div>
         <div className="button-panel">
-          <button className="button-next">Next</button>
+          <button
+            className="button-next"
+            disabled={!this.isFormCommitable() || isTimeOver}
+            onClick={this.handleClickNextForm}
+          >
+            Next
+          </button>
         </div>
       </div>
     );
--- src/Step.js
+++ src/Step.js
@@ -4,11 +4,11 @@ import "./Step.css";
 class Step extends PureComponent {
   handleClick = () => {};
   render() {
-    const { title, index } = this.props;
+    // const { title, index } = this.props;
     return (
       <div className="step">
         <div className="step__number">{this.props.number}</div>
-        <div className="step__title">{this.props.title}</div>
+        <div className="step__title">{this.props.children}</div>
       </div>
     );
   }
